c341e95ec9754d3e7b139cd0eff68c8355e32515
AlMoatasim Billah <77535107+Almoatasimbillah@users.noreply.github.com>
2026-02-22 22:39:50 +0200

    Create script.js
    1st iteration

---
+++ script.js
@@ -0,0 +1,15 @@
+// Initialize Animations
+AOS.init({
+    duration: 1000,
+    once: true,
+});
+
+// Smooth Scroll for Navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
